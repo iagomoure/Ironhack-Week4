71520bd58062e592ceaa7187289be9282c4ef05d
iagomoure <iago_moure@yahoo.es>
2015-11-16 18:05:36 +0100

    Add initial structure Rails Address Book and final changes in timetracking

--- Day1/timetracking/db/schema.rb
+++ Day1/timetracking/db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116102507) do
+ActiveRecord::Schema.define(version: 20151116154922) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
 end
--- Day1/timetracking/db/seeds.rb
+++ Day1/timetracking/db/seeds.rb
@@ -1,3 +1,3 @@
 (1..25).each do |index|
-	Project.create(name: "Project #{index}")
+	Project.create(name: "Project #{index}",description: "Content #{index}")
 end
