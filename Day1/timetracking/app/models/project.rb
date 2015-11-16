a5a4a3223d0cb0a5c6c5c04fcde33ab1a2731910
iagomoure <iago_moure@yahoo.es>
2015-11-16 14:50:32 +0100

    Add seeds

--- Day1/timetracking/app/models/project.rb
+++ Day1/timetracking/app/models/project.rb
@@ -3,4 +3,9 @@ class Project < ActiveRecord::Base
 	def self.iron_find(params)
 		where(id: params).first
 	end
+
+	def self.clean_old
+    date = Time.now.midnight - 1.week
+    where('created_at < ?, date').destroy_all
+	end
 end
