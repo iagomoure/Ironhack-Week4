38765a624a19ac88a9293e308c79f84d65121514
iagomoure <iago_moure@yahoo.es>
2015-11-18 15:34:39 +0100

    display validates error, helpers routes and redirect_to

--- Day1/timetracking/app/controllers/entries_controller.rb
+++ Day1/timetracking/app/controllers/entries_controller.rb
@@ -7,4 +7,28 @@ class EntriesController < ApplicationController
 		@entries = @project.entries.where(date: date.beginning_of_month..date.end_of_month)
 		@total_hours = @project.total_hours_in_month(date.month,date.year)
 	end
+
+	def new
+		@project = Project.find(params[:project_id])
+		#Igual que Entry.new pero de esta menera en vez de ser todos 
+		#los elementos nil el project_id ya está relleno
+		@entry = @project.entries.new 
+	end
+
+	def create
+		@project = Project.find(params[:project_id])
+		@entry = @project.entries.new(entry_params)
+		if @entry.save
+			redirect_to action: 'index', controller: 'entries', project_id: @project.id			
+		else		
+			render "new"
+		end	
+	end
+
+	private 
+
+	def entry_params
+		params.require(:entry).permit(:hours, :minutes, :date)
+	end
+
 end
--- Day1/timetracking/config/routes.rb
+++ Day1/timetracking/config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   get '/' => 'site#home'
   get '/contact' => 'site#contact'
-  get '/projects' => 'projects#index'
+  
+  #get '/projects' => 'projects#index' 
+  #Convención => index para mostar listados
+  #
+  #get 'projects/:id' => 'projects#show' 
+  #Convención => show para mostrar detalle
+  #get 'projects/:project_id/entries' => 'entries#index'
+  #
+  #get 'projects/:project_id/entries/new' => 'entries#new' 
+  #Convención => new para crear objetos
 
-  get 'projects/:id' => 'projects#show'
-  get 'projects/:project_id/entries' => 'entries#index'
+  #helper resources y sustituye a todo lo de arriba
+  resources :projects, only: [:index, :show]do
+  	resources :entries, only: [:index, :new, :create]
+  end
 end
